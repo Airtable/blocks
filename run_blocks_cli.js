867d631ecf84d43ad94dd41274153edda8696819
utk9 <utkarshlamba.2@gmail.com>
2017-11-28 15:57:56 -0800

    Add block run command

---
+++ run_blocks_cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+'use  strict';
+
+const _ = require('lodash');
+const BlockBuilderServer = require('./lib/block_builder_server');
+
+const Commands = {
+    run: 'run',
+    push: 'push',
+    pull: 'pull',
+};
+
+function _exitWithError(message) {
+    console.log(message);
+    process.exit(1);
+}
+
+const command = process.argv[2];
+if (!command || !_.includes(_.values(Commands), command)) {
+    _exitWithError('Please provide a valid command');
+}
+
+switch (command) {
+    case Commands.run:
+        const port = parseInt(process.argv[3]);
+        if (!port || isNaN(port)) {
+            _exitWithError('Please provide a valid port number');
+        }
+        const blockBuilderServer = new BlockBuilderServer(port);
+        blockBuilderServer.start();
+        break;
+    default:
+        _exitWithError('Not implemented yet');
+}
