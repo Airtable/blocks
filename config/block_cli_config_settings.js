0fefbb7fbb267bbc51897a662ee330ea65c8f88a
utk9 <utkarshlamba.2@gmail.com>
2017-11-30 17:50:31 -0800

    Add missing config var and drop an unnecessary .js from a require

--- config/block_cli_config_settings.js
+++ config/block_cli_config_settings.js
@@ -8,6 +8,7 @@ module.exports = {
         GLOBAL_SDK_VARIABLE_NAME: '_airtableBlockSdk',
         GLOBAL_REACT_VARIABLE_NAME: '_airtableReact',
         GLOBAL_REACT_DOM_VARIABLE_NAME: '_airtableReactDOM',
+        GLOBAL_RUN_BLOCK_FUNCTION_NAME: '_airtableRunBlock',
 
         // Wrapper file for blocks frontend code
         CLIENT_WRAPPER_FILE_NAME: 'block_client_wrapper.js',
