438b8e31a3f242814266a46f8a58dcc266126ee3
✨ Alex ✨ <alex@dytry.ch>
2018-04-19 19:03:46 +0100

    run prettier

--- run_block_cli.js
+++ run_block_cli.js
@@ -35,28 +35,31 @@ function _exitWithError(message, err) {
 }
 
 function startBlockBundleServerNgrok(blockBundleServer, port) {
-    return blockBundleServer
-        .startAsync(port)
-        .then(() => new Promise((resolve, reject) => {
-            require('ngrok').connect(port, (err, url) => {
-                if (err) reject(err);
-                resolve(url);
-            });
-        }))
+    return blockBundleServer.startAsync(port).then(
+        () =>
+            new Promise((resolve, reject) => {
+                require('ngrok').connect(port, (err, url) => {
+                    if (err) reject(err);
+                    resolve(url);
+                });
+            }),
+    );
 }
 
 function startBlockBundleServerLocal(blockBundleServer, port) {
-    return blockBundleServer
-        .startLocalAsync(port)
-        .then(() => {
-            const url = `https://localhost:${port}`;
-            console.log('Local mode: serving self-signed https on localhost');
-            console.log('If this is the first time you\'re running this command in local mode, you need to do some extra setup in your browser:')
-            console.log(`  - Firefox: go to https://localhost:${port} and add an ssl exception`);
-            console.log('  - Chrome: go to chrome://flags/#allow-insecure-localhost and click enable. Restart your browser');
-            console.log('');
-            return url;
-        })
+    return blockBundleServer.startLocalAsync(port).then(() => {
+        const url = `https://localhost:${port}`;
+        console.log('Local mode: serving self-signed https on localhost');
+        console.log(
+            "If this is the first time you're running this command in local mode, you need to do some extra setup in your browser:",
+        );
+        console.log(`  - Firefox: go to https://localhost:${port} and add an ssl exception`);
+        console.log(
+            '  - Chrome: go to chrome://flags/#allow-insecure-localhost and click enable. Restart your browser',
+        );
+        console.log('');
+        return url;
+    });
 }
 
 function startBlockBundleServer(blockBundleServer, port, shouldUseLocalhost) {
@@ -81,15 +84,17 @@ function startBlockBundleServer(blockBundleServer, port, shouldUseLocalhost) {
                 promptAsync({
                     name: 'port',
                     description: `Port ${port} is taken, please provide an alternative port to run on`,
-                }).then(result => {
-                    const newPort = result.port;
-                    if (isNaN(newPort)) {
-                        _exitWithError('Invalid port number');
-                    }
-                    startBlockBundleServer(blockBundleServer, newPort, shouldUseLocalhost);
-                }).catch(innerErr => {
-                    _exitWithError(innerErr.message);
-                });
+                })
+                    .then(result => {
+                        const newPort = result.port;
+                        if (isNaN(newPort)) {
+                            _exitWithError('Invalid port number');
+                        }
+                        startBlockBundleServer(blockBundleServer, newPort, shouldUseLocalhost);
+                    })
+                    .catch(innerErr => {
+                        _exitWithError(innerErr.message);
+                    });
             } else {
                 _exitWithError(err.message);
             }
@@ -99,7 +104,10 @@ function startBlockBundleServer(blockBundleServer, port, shouldUseLocalhost) {
 const runBlocksCli = function runBlocksCli() {
     const config = yargsOuter
         .usage('Usage: block <command> [options]')
-        .command(`${Commands.CLONE} <blockIdentifier> <blockDirPath>`, 'Clone a block from Airtable')
+        .command(
+            `${Commands.CLONE} <blockIdentifier> <blockDirPath>`,
+            'Clone a block from Airtable',
+        )
         .command(Commands.RUN, 'Build and run a block')
         .command(Commands.PUSH, 'Push changes to Airtable')
         .command(Commands.PULL, 'Pull changes from Airtable')
@@ -112,7 +120,8 @@ const runBlocksCli = function runBlocksCli() {
             default: false,
         })
         .option('local', {
-            description: 'Run blocks locally on with a self-signed certificate instead of through ngrok.io',
+            description:
+                'Run blocks locally on with a self-signed certificate instead of through ngrok.io',
             type: 'boolean',
             default: false,
         })
@@ -139,8 +148,7 @@ const runBlocksCli = function runBlocksCli() {
         .example('block push')
         .example('block pull')
         .example('block rename-entry newModuleName')
-        .help('help')
-        .argv;
+        .help('help').argv;
 
     const command = config._[0] || '';
     if (command === Commands.RUN) {
@@ -154,63 +162,84 @@ const runBlocksCli = function runBlocksCli() {
         promptAsync({
             name: 'apiKey',
             description: `Please enter your API key. You can generate one at https://${domain}/account`,
-        }).then(result => {
-            const blockCloneAsync = require('./lib/block_clone');
-            return blockCloneAsync(environment, config.appId, config.blockId, config.blockDirPath, result.apiKey);
-        }).then(() => {
-            console.log(`Block cloned in ${config.blockDirPath}`);
-        }).catch(err => {
-            _exitWithError(err.message);
-        });
+        })
+            .then(result => {
+                const blockCloneAsync = require('./lib/block_clone');
+                return blockCloneAsync(
+                    environment,
+                    config.appId,
+                    config.blockId,
+                    config.blockDirPath,
+                    result.apiKey,
+                );
+            })
+            .then(() => {
+                console.log(`Block cloned in ${config.blockDirPath}`);
+            })
+            .catch(err => {
+                _exitWithError(err.message);
+            });
     } else if (command === Commands.PUSH) {
         const blockPushAsync = require('./lib/block_push');
-        blockPushAsync({shouldForceUpdate: config.force}).then(() => {
-            console.log('Remote block updated');
-        }).catch(err => {
-            _exitWithError(err.message);
-        });
+        blockPushAsync({shouldForceUpdate: config.force})
+            .then(() => {
+                console.log('Remote block updated');
+            })
+            .catch(err => {
+                _exitWithError(err.message);
+            });
     } else if (command === Commands.PULL) {
         const blockPullAsync = require('./lib/block_pull');
-        blockPullAsync().then(() => {
-            console.log('Local block updated');
-        }).catch(err => {
-            _exitWithError(err.message);
-        });
+        blockPullAsync()
+            .then(() => {
+                console.log('Local block updated');
+            })
+            .catch(err => {
+                _exitWithError(err.message);
+            });
     } else if (command === Commands.FORMAT) {
         const findBlockDirPathAsync = require('./lib/find_block_dir_path');
         const blockFormat = require('./lib/block_format');
-        findBlockDirPathAsync().then(blockDirPath => {
-            const setUpDevToolsIfNeededSync = require('./lib/set_up_dev_tools_if_needed_sync');
-            const didSetUpDevTools = setUpDevToolsIfNeededSync(blockDirPath);
-            if (didSetUpDevTools) {
-                console.log('Dev dependencies updated. Please run `npm install` and try again.');
-                process.exit(1);
-            }
+        findBlockDirPathAsync()
+            .then(blockDirPath => {
+                const setUpDevToolsIfNeededSync = require('./lib/set_up_dev_tools_if_needed_sync');
+                const didSetUpDevTools = setUpDevToolsIfNeededSync(blockDirPath);
+                if (didSetUpDevTools) {
+                    console.log(
+                        'Dev dependencies updated. Please run `npm install` and try again.',
+                    );
+                    process.exit(1);
+                }
 
-            console.log('Formatting...');
-            blockFormat(blockDirPath);
-            console.log('Done');
-        }).catch(err => {
-            _exitWithError(err.message);
-        });
+                console.log('Formatting...');
+                blockFormat(blockDirPath);
+                console.log('Done');
+            })
+            .catch(err => {
+                _exitWithError(err.message);
+            });
     } else if (command === Commands.LINT) {
         const findBlockDirPathAsync = require('./lib/find_block_dir_path');
         const blockLint = require('./lib/block_lint');
-        findBlockDirPathAsync().then(blockDirPath => {
-            const setUpDevToolsIfNeededSync = require('./lib/set_up_dev_tools_if_needed_sync');
-            const didSetUpDevTools = setUpDevToolsIfNeededSync(blockDirPath);
-            if (didSetUpDevTools) {
-                console.log('Dev dependencies updated. Please run `npm install` and try again.');
-                process.exit(1);
-            }
+        findBlockDirPathAsync()
+            .then(blockDirPath => {
+                const setUpDevToolsIfNeededSync = require('./lib/set_up_dev_tools_if_needed_sync');
+                const didSetUpDevTools = setUpDevToolsIfNeededSync(blockDirPath);
+                if (didSetUpDevTools) {
+                    console.log(
+                        'Dev dependencies updated. Please run `npm install` and try again.',
+                    );
+                    process.exit(1);
+                }
 
-            console.log('Linting...');
-            const lint = blockLint(blockDirPath);
-            const report = lint.report;
-            console.log(lint.formatter(report.results));
-        }).catch(err => {
-            _exitWithError(err.message);
-        });
+                console.log('Linting...');
+                const lint = blockLint(blockDirPath);
+                const report = lint.report;
+                console.log(lint.formatter(report.results));
+            })
+            .catch(err => {
+                _exitWithError(err.message);
+            });
     } else if (command === Commands.RENAME_ENTRY) {
         const updateEntryModuleName = require('./lib/update_entry_module_name');
         try {
