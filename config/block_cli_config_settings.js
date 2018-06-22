76502d665bace767c994dc9ccd7a1e37852f115d
Dane Nespoli <danenespoli@gmail.com>
2018-06-21 17:07:50 -0700

    write bundle and wrapper to build dir

--- config/block_cli_config_settings.js
+++ config/block_cli_config_settings.js
@@ -11,18 +11,18 @@ module.exports = {
     GLOBAL_REACT_DOM_SERVER_VARIABLE_NAME: '_airtableReactDOMServer',
     GLOBAL_RUN_BLOCK_FUNCTION_NAME: '_airtableRunBlock',
 
-    // Wrapper file for blocks frontend code.
-    CLIENT_WRAPPER_FILE_NAME: 'block_client_wrapper.js',
-
     // Metadata file for the block cli.
     BLOCK_FILE_NAME: 'block.json',
 
     // File to store the airtable api key.
     AIRTABLE_API_KEY_FILE_NAME: '.airtableAPIKey',
 
+    // Build directory for frontend bundle and transpiled backend code.
+    BUILD_DIR: 'build',
+
+    // Wrapper file for blocks frontend code.
+    CLIENT_WRAPPER_FILE_NAME: 'block_client_wrapper.js',
+
     // Bundle file.
     BUNDLE_FILE_NAME: 'bundle.js',
-
-    // Transpiled backend code directory.
-    BUILD_DIR: 'build',
 };
