69d38c1847025bf42f0dbc74f86cea28b49e9fc3
JB Bakst <jbbakst@gmail.com>
2019-03-19 18:52:19 -0700

    Flow
    Summary:
    Depends on D8717
    
    Adds flow to the blocks-cli repo
    
    Test Plan: - Verify that running the blocks-cli still works properly
    
    Reviewers: kasrak, EvanHahn
    
    Reviewed By: EvanHahn
    
    Differential Revision: https://airtable.phacility.com/D8718

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,9 +1,10 @@
+// @flow
 const Commands = {
-    RUN: 'run',
-    CLONE: 'clone',
-    PUSH: 'push',
-    PULL: 'pull',
-    RENAME_ENTRY: 'rename-entry',
+    RUN: ('run': 'run'),
+    CLONE: ('clone': 'clone'),
+    PUSH: ('push': 'push'),
+    PULL: ('pull': 'pull'),
+    RENAME_ENTRY: ('rename-entry': 'rename-entry'),
 };
 
 module.exports = Commands;
