Airtable/blocks

commit b7bf30169a043ade339aeef7931aa2b21c05f3d1

Use ms from Unix epoch for tmp build directory name
Summary:
For build output directory name, use the number of millseconds (Date.prototype.getTime) since the
Unix Epoch instead of the timestamp given by `Date.prototype.toISOString`.

Reason:
* `Date.prototype.toISOString` returns a format like `"2019-04-22T16:57:07.187Z"` which includes the colon `:` character. The colon character `:` is a reserved character for certain operating systems like Windows (see https://docs.microsoft.com/en-us/windows/desktop/fileio/naming-a-file#file-and-directory-names)
* `Date.prototype.getTime` also always uses UTC for time representation, and is the number of milliseconds since Unix Epoch (NOTE: this is slightly different from Unix Time because Unix Time is actually the number of seconds since epoch, but JS returns the number of ms). If needed, we can change the granularity to seconds instead of ms.

Test Plan: No QA Needed

Reviewers: jbbakst

Reviewed By: jbbakst

Subscribers: #blocks

Differential Revision: https://airtable.phacility.com/D9148

src/commands/build.js
@@ -5,8 +5,8 @@ const path = require('path');
 import type {Argv} from 'yargs';
 
 function _getOutputDirPath(): string {
-    const isoDateString = new Date().toISOString();
-    return path.join('/tmp', 'build', isoDateString);
+    const timestamp = new Date().getTime();
+    return path.join('/tmp', 'build', timestamp);
 }
 
 async function runCommandAsync(argv: Argv): Promise<void> {

commit 15f38db4d88da83c4d56c25a1aa42ae0915067bc

fix directory path to be string
Summary:
Introduced regression with D9148. The arg to `path` should be a string, but was passing in a number.

Tested locally.

Test Plan: No QA Needed

Reviewers: jbbakst

Reviewed By: jbbakst

Subscribers: #blocks

Differential Revision: https://airtable.phacility.com/D9171

src/commands/build.js
@@ -5,8 +5,8 @@ const path = require('path');
 import type {Argv} from 'yargs';
 
 function _getOutputDirPath(): string {
-    const timestamp = new Date().getTime();
-    return path.join('/tmp', 'build', timestamp);
+    const timestampString = new Date().getTime().toString();
+    return path.join('/tmp', 'build', timestampString);
 }
 
 async function runCommandAsync(argv: Argv): Promise<void> {