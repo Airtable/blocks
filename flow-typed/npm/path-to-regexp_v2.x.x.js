37f2ceac28736736d06107c5f6141a865174cae1
Richard Sinn <richard.sinn@airtable.com>
2019-04-29 16:28:57 -0700

    update flow-typed definition for path-to-regexp dep
    Summary:
    The `flow-typed` definition for the `path-to-regexp` lib was wrong (see https://github.com/flow-typed/flow-typed/issues/3274).
    Fix has been made in the `flow-typed` repo, so updating the definition in our repo.
    
    Test Plan:
    No QA Needed
    Tested `yarn flow` locally
    
    Reviewers: jbbakst
    
    Reviewed By: jbbakst
    
    Subscribers: #blocks
    
    Differential Revision: https://airtable.phacility.com/D9269

--- flow-typed/npm/path-to-regexp_v2.x.x.js
+++ flow-typed/npm/path-to-regexp_v2.x.x.js
@@ -1,34 +1,34 @@
-// flow-typed signature: 34ce6a4306af61160b2da155b6a0da81
-// flow-typed version: 8cfbec541d/path-to-regexp_v2.x.x/flow_>=v0.25.x
+// flow-typed signature: 8efce3fb6b6a08c019aaf24a67c8f474
+// flow-typed version: 256ef26117/path-to-regexp_v2.x.x/flow_>=v0.25.x
 
-declare module "path-to-regexp" {
+declare module 'path-to-regexp' {
   declare export type Key = {
-    name: string | number;
-    prefix: string;
-    delimiter: string;
-    optional: boolean;
-    repeat: boolean;
-    pattern: string;
-    partial: boolean;
-  }
+    name: string | number,
+    prefix: string,
+    delimiter: string,
+    optional: boolean,
+    repeat: boolean,
+    pattern: string,
+    partial: boolean,
+  };
 
   declare export type RegExpOptions = {
-    sensitive?: boolean;
-    strict?: boolean;
-    end?: boolean;
-    start?: boolean;
-    delimiter?: string;
-    endsWith?: string | string[];
-  }
+    sensitive?: boolean,
+    strict?: boolean,
+    end?: boolean,
+    start?: boolean,
+    delimiter?: string,
+    endsWith?: string | string[],
+  };
 
   declare export type ParseOptions = {
-    delimiter?: string;
-    delimiters?: string | string[];
-  }
+    delimiter?: string,
+    delimiters?: string | string[],
+  };
 
-  declare type PathFunctionOptions = {
-    encode?: (value: string, token: Key) => string;
-  }
+  declare export type PathFunctionOptions = {
+    encode?: (value: string, token: Key) => string,
+  };
 
   declare export type Token = string | Key;
 
@@ -39,21 +39,15 @@ declare module "path-to-regexp" {
     options?: PathFunctionOptions
   ) => string;
 
-  declare export var parse: (path: string, options?: ParseOptions) => Token[];
-
-  declare export var compile: (path: string, options?: {}) => PathFunction;
-
-  declare export var tokensToFunction: (tokens: Token[]) => PathFunction;
-
-  declare export var tokensToRegExp: (
-    tokens: Token[],
-    keys?: Key[],
-    options?: RegExpOptions
-  ) => RegExp;
-
-  declare export default (
-    path: Path,
-    keys?: Key[],
-    options?: RegExpOptions & ParseOptions
-  ) => RegExp;
+  declare module.exports: {
+    (path: Path, keys?: Key[], options?: RegExpOptions & ParseOptions): RegExp,
+    parse: (path: string, options?: ParseOptions) => Token[],
+    compile: (path: string, options?: {}) => PathFunction,
+    tokensToFunction: (tokens: Token[]) => PathFunction,
+    tokensToRegExp: (
+      tokens: Token[],
+      keys?: Key[],
+      options?: RegExpOptions
+    ) => RegExp,
+  };
 }
