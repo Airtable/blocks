b28f2e10873d6de30259d11e38d6d12ced954f1c
Kasra Kyanzadeh <kasra@airtable.com>
2019-06-11 17:18:29 -0700

    init: bump sdk version to 0.0.7

--- src/commands/init.js
+++ src/commands/init.js
@@ -88,7 +88,7 @@ async function initBlockAsync(
 
     // Create a minimal package json so the user can yarn install.
     const defaultDependencies = {
-        '@airtable/blocks': '^0.0.6',
+        '@airtable/blocks': '^0.0.7',
         react: '^16.8.0',
         'react-dom': '^16.8.0',
     };
