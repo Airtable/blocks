dfc2b7eaa8dd9caed3175629738e74b70018eb47
Kasra Kyanzadeh <kasra@airtable.com>
2019-06-11 17:17:36 -0700

    publish v0.0.7

--- dist/cjs/sdk.js
+++ dist/cjs/sdk.js
@@ -211,6 +211,6 @@ function () {
   return BlockSdk;
 }();
 
-(0, _defineProperty2.default)(BlockSdk, "VERSION", "0.0.6");
+(0, _defineProperty2.default)(BlockSdk, "VERSION", "0.0.7");
 var _default = BlockSdk;
 exports.default = _default;
