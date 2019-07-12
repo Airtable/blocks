4903cf84a8bea548973207a0db5582694b70fd67
✨ Alex ✨ <alex@dytry.ch>
2019-07-12 17:45:05 +0100

    sdk.CollaboratorToken: fix lint error
    Summary: Fix a lint error in CollaboratorToken.
    
    Test Plan: No QA needed
    
    Reviewers: minicat
    
    Reviewed By: minicat
    
    Differential Revision: https://airtable.phacility.com/D10591

--- packages/sdk/src/ui/collaborator_token.js
+++ packages/sdk/src/ui/collaborator_token.js
@@ -1,6 +1,7 @@
 // @flow
 import PropTypes from 'prop-types'; // TODO(kasra): don't depend on liveapp components.
 import * as React from 'react';
+import {has} from '../private_utils';
 import getSdk from '../get_sdk';
 
 const {u} = window.__requirePrivateModuleFromAirtable('client_server_shared/hu');
@@ -63,7 +64,7 @@ const CollaboratorToken = (props: CollaboratorTokenProps) => {
     // formatting it nicely.
     const userInfoById = getSdk().__appInterface.getCollaboratorInfoById();
     const userObj =
-        userInfoById && collaborator.id && userInfoById.hasOwnProperty(collaborator.id)
+        userInfoById && collaborator.id && has(userInfoById, collaborator.id)
             ? userInfoById[collaborator.id]
             : null;
     const userObjFormattedForPublicApiV2 = userObj
