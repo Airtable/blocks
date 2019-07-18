f2ea0b9e18d6cf2a6c3a402562e1d1977c343ad8
✨ Alex ✨ <alex@dytry.ch>
2019-07-18 20:13:40 +0100

    setup airbuild
    Summary:
    Adds airbuild.js to configure airbuild for this repository. Also tweaks some of our test commands to make sure everything that should get run actually gets run.
    
    See D10693 for the airbuild side of this.
    
    Test Plan: QA can skip
    
    Reviewers: EvanHahn, jbbakst, kasrak, richsinn
    
    Reviewed By: richsinn
    
    Subscribers: mark.hahnenberg
    
    Differential Revision: https://airtable.phacility.com/D10694

---
+++ airbuild.js
@@ -0,0 +1,27 @@
+// @flow
+
+const airbuild = {
+    nodejsVersion: '8.15.0',
+    yarnVersion: '1.13.0',
+    mysqlVersion: 'n/a',
+    testCommand: 'yarn test && echo "RUN_ALL_TESTS PASSED"',
+    environmentVariables: {
+        NODE_ENV: 'test',
+    },
+    options: {
+        nodeModuleInstallCommand: 'yarn',
+        postRestoreCachedNodeModulesCommand: 'yarn',
+        directoryCaching: false,
+        disableNodeModulesCaching: true,
+        disableWipeNodeModules: true,
+    },
+    directoriesWithNodeModules: [],
+    directoriesToCacheByAction: {
+        transpile: [],
+        compileAssets: [],
+    },
+    testDefinitions: [],
+    mochaGlobalsToIgnore: [],
+};
+
+module.exports = airbuild;
