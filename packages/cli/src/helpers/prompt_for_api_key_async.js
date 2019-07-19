38166e610929a5b0652bb676baefaecd143ae17b
Richard Sinn <richard.sinn@airtable.com>
2019-07-18 17:13:26 -0700

    cli: Validations around prompt API Key
    Summary:
    Provide some validations around our CLI prompt for API Key input. This fix was inspired while testing on the Windows OS - I couldn't tell if I was copying and pasting the API Key correctly into the terminal.
    
    Changes to `inquirer`:
    * Config to `mask` passwords (to  visually aide the user that characters are being inputted)
    ** Prior to this diff, there was no UI feedback (everything was hidden) - making it especially hard if you're copy&pasting in the API key
    * Add validation to check for:
    ** empty string
    ** input for API Key token starts with `key`
    
    Screenshots:
    * API Key input with mask characters (`*` asterisk char) {F125990}
    * API Key empty string error {F125989}
    * API Key incorrect format error (note: `inquirer` blanks out the input on error) {F125991}
    
    Tested manually.
    
    Test Plan: No QA needed
    
    Reviewers: jbbakst, jichu4n, somehats
    
    Reviewed By: somehats
    
    Differential Revision: https://airtable.phacility.com/D10704

--- packages/cli/src/helpers/prompt_for_api_key_async.js
+++ packages/cli/src/helpers/prompt_for_api_key_async.js
@@ -5,6 +5,18 @@ const {AIRTABLE_ACCOUNT_URL} = require('../config/block_cli_config_settings');
 async function promptForApiKeyAsync(): Promise<string> {
     const {apiKey} = await inquirer.prompt({
         type: 'password',
+        mask: '*',
+        validate: input => {
+            if (input === '') {
+                return 'API Key cannot be blank!';
+            }
+
+            if (!input.startsWith('key')) {
+                return 'Incorrect API Key!';
+            }
+
+            return true;
+        },
         name: 'apiKey',
         message: `Please enter your API key. You can generate one at ${AIRTABLE_ACCOUNT_URL}`,
     });
