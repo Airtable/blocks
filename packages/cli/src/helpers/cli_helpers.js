03e9f9e8906aac67f65308634b37affa790c4d62
Richard Sinn <richard.sinn@airtable.com>
2019-07-31 09:29:04 -0700

    cli: minor flowify cli_helpers.js
    Summary: Flowify the `cli_helpers.js` file.
    
    Test Plan: No QA needed
    
    Reviewers: jichu4n
    
    Reviewed By: jichu4n
    
    Differential Revision: https://airtable.phacility.com/D10938

--- packages/cli/src/helpers/cli_helpers.js
+++ packages/cli/src/helpers/cli_helpers.js
@@ -1,7 +1,8 @@
+// @flow
 /* eslint-disable no-console */
 
 const helpers = {
-    exitWithError(message, err) {
+    exitWithError(message: string, err?: Error): void {
         console.error('Error:', message);
         if (err) {
             console.error(err.stack);
