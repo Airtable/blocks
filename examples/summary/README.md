46f768010a97432eafa96bcdf25c245629dc180e
Mary Rose Cook <mary@airtable.com>
2019-07-31 17:37:51 -0700

    Add Summary block docs example block
    Summary: ^ and see block readme
    
    Test Plan: QA can skip
    
    Reviewers: kasrak, jbbakst
    
    Reviewed By: kasrak
    
    Subscribers: somehats
    
    Differential Revision: https://airtable.phacility.com/D10899

---
+++ examples/summary/README.md
@@ -0,0 +1,30 @@
+# Summary block
+
+This example block lets the user pick a table (e.g. Opportunities), a field (e.g. Estimated Value)
+and a function to use to summarize that field (e.g. max).
+
+The code shows how to use the built-in summary functions like average, max etc. to summarize a table
+field.
+
+It also includes examples of:
+
+-   Using the built-in components to let users choose a table or field.
+
+-   Using the built-in `Select` component to let the user choose from a custom set of options.
+
+## How to run this block
+
+1. Create a new base using the
+   [Sales CRM template](https://airtable.com/templates/sales-and-customers/expvjTzYAZareV1pt/sales-crm).
+
+2. Create a new block in your new base (see the
+   [setup guide](https://airtable.github.io/blocks/guides/setup/)).
+
+3. Copy the code from the `frontend` directory of this block to the `frontend` directory of your new
+   block.
+
+4. From the root of your new block, run `block run`.
+
+## Screenshot
+
+![Block screenshot](images/block_screenshot.png?raw=true)
---
+++ examples/summary/frontend/index.js
@@ -0,0 +1,63 @@
+import {
+    initializeBlock,
+    useBase,
+    useRecords,
+    TablePicker,
+    FieldPicker,
+    Select,
+} from '@airtable/blocks/ui';
+import React, {useState} from 'react';
+
+const TABLE_NAME = 'Opportunities';
+
+const FIELD_NAME = 'Estimated Value';
+
+function SummaryBlock() {
+    const base = useBase();
+
+    const table = base.getTableByName(TABLE_NAME);
+
+    const field = table.getFieldByName(FIELD_NAME);
+
+    const records = useRecords(table.selectRecords({fields: [field]}));
+
+    // `Field` objects have an `availableAggregators` property that
+    // returns a list of aggregation functions that are valid for this
+    // field type
+    const availableAggregators = field.availableAggregators;
+
+    return (
+        <div style={{padding: 16}}>
+            {availableAggregators
+                // Every field has a 'None' aggregator which outputs a
+                // blank. It's not very interesting to look at, so we
+                // filter it out.
+                .filter(aggregator => aggregator.key !== 'none')
+                .map((aggregator, key) => (
+                    <Aggregation
+                        key={key}
+                        field={field}
+                        aggregator={aggregator}
+                        records={records}
+                    />
+                ))}
+        </div>
+    );
+}
+
+function Aggregation({field, aggregator, records}) {
+    // `aggregateToString()` returns a human-readable string for the
+    // aggregate value. If you just want a number, you can call
+    // `aggregate()`
+
+    return (
+        <div style={{marginBottom: 16}}>
+            {aggregator.displayName}
+            <div style={{fontSize: 14, fontWeight: 'bold'}}>
+                {aggregator.aggregateToString(records, field)}
+            </div>
+        </div>
+    );
+}
+
+initializeBlock(() => <SummaryBlock />);
