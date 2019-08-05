b24707bb01973fcf42445cc7d645942d05bb91b2
evanhahn and maryrosecook <evan.hahn@airtable.com>
2019-08-05 13:55:58 -0500

    docs: improve release script
    Summary:
    This beefs up our release script. It:
    
    1. moves the script into its own file
    2. works even if you don't have the `public` remote (or if `public` points to the wrong place)
    3. removes `gh-pages`'s `.cache` directory, which can cause problems
    4. adds the `--git` option to `gh-pages`, which sometimes fails to find the `git` executable
    
    Test Plan: QA can skip
    
    Reviewers: somehats, maryrosecook
    
    Reviewed By: maryrosecook
    
    Differential Revision: https://airtable.phacility.com/D11038

---
+++ docs/bin/release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
+
+docs_bin_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+docs_dir="$(cd "$docs_bin_dir/.." && pwd)"
+project_root_dir="$(cd "$docs_dir/.." && pwd)"
+
+cd "$docs_dir"
+
+"$project_root_dir/bin/check-repo-for-release"
+
+yarn run gatsby build --prefix-paths
+
+# NOTE(evanhahn) gh-pages's .cache directy can cause problems, so we remove it.
+rm -rf "$docs_dir/node_modules/gh-pages/.cache"
+
+# NOTE(evanhahn) Yarn is unable to find `gh-pages` for some reason, so we use
+# this trick instead of calling `yarn run gh-pages`.
+"$(yarn bin)/gh-pages" \
+  --git "$(command -v git)" \
+  --dist 'public' \
+  --message 'Update docs' \
+  --repo 'git@github.com:Airtable/blocks.git'
