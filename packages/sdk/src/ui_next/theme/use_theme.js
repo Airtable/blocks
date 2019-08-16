f38877291f9eb0ef8fd74db0618af9ca6e95897c
jay-airtable <jay@airtable.com>
2019-08-16 12:36:57 -0700

    sdk: useStyledSystem
    Summary:
    This is the first diff in a series of diffs that will implement the Box UI primitive.
    
    See  https://airtable.phacility.com/D11197  for how this will be used in practice.
    
    ## This diff is part of a series of stacking diffs:
    
    1. `sdk-useStyledSystem`  https://airtable.phacility.com/D11141 (current)
    2. `sdk-system` https://airtable.phacility.com/D11154
    3. `sdk-boxComp` https://airtable.phacility.com/D11197
    
    Test Plan:
    N/A. I will include steps once we have some more stuff that we can actually run.
    
    I can investigate how to add tests once we got some style parsers in a follow up diff, the stories that will be added in the future should also help with testing.
    
    Reviewers: s2tephen, somehats
    
    Reviewed By: s2tephen, somehats
    
    Subscribers: billylittlefield
    
    Differential Revision: https://airtable.phacility.com/D11141

---
+++ packages/sdk/src/ui_next/theme/use_theme.js
@@ -0,0 +1,5 @@
+// @flow
+import {useContext} from 'react';
+import ThemeContext from './theme_context';
+const useTheme = () => useContext(ThemeContext);
+export default useTheme;
---
+++ packages/sdk/src/ui_next/use_styled_system.js
@@ -0,0 +1,24 @@
+// @flow
+import {css} from 'emotion';
+import useTheme from './theme/use_theme';
+
+// Imitate the Parser type from `@styled-system/core`.
+type Parser<T> = {
+    (props: T): {},
+    config: {+[string]: mixed},
+    propNames: Array<string>,
+    cache: {},
+};
+
+/** @private */
+export default function useStyledSystem<T>(styleProps: T, parser: Parser<T>): string {
+    const theme = useTheme();
+
+    // Add the theme to the style props because that's how the parser expects it.
+    const styles = parser({...styleProps, theme});
+
+    // At this point `styles` is still an object, we need to turn it into a class name.
+    const classNameForStyleProps = css(styles);
+
+    return classNameForStyleProps;
+}
