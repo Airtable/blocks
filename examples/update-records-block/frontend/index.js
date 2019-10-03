fec4f0da893474b36dc2bf06764713bcff7e2135
Stephen Suen <stephen.suen@airtable.com>
2019-10-03 13:28:14 -0700

    update_records: use box
    Summary: Converts the Update Records example block to styled system
    
    Test Plan: Verify the block still works as expected
    
    Reviewers: billylittlefield, jay-ransijn
    
    Reviewed By: billylittlefield
    
    Differential Revision: https://airtable.phacility.com/D12271

--- examples/update-records-block/frontend/index.js
+++ examples/update-records-block/frontend/index.js
@@ -1,4 +1,5 @@
 import {
+    Box,
     Button,
     initializeBlock,
     useBase,
@@ -56,16 +57,18 @@ function UpdateRecordsBlock() {
 // Container element which centers its children.
 function Container({children}) {
     return (
-        <div
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '100vh',
-            }}
+        <Box
+            position="absolute"
+            top={0}
+            bottom={0}
+            left={0}
+            right={0}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
         >
             {children}
-        </div>
+        </Box>
     );
 }
 
