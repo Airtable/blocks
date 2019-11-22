ac306f790b23206a69e5e0e8a41eb5945b1e7137
Emma Yeap <emma@airtable.com>
2019-11-22 11:27:39 -0800

    sdk: delete record must exist invariant
    Summary: New SDK version of D13289
    
    Test Plan: No QA required. I'll test this against a custom block that watches for record coloring
    
    Reviewers: somehats
    
    Reviewed By: somehats
    
    Differential Revision: https://airtable.phacility.com/D13290

--- packages/sdk/src/models/view_data_store.ts
+++ packages/sdk/src/models/view_data_store.ts
@@ -296,10 +296,11 @@ class ViewDataStore extends AbstractModelWithAsyncData<ViewData, WatchableViewDa
                 if (this.parentRecordStore.isRecordMetadataLoaded) {
                     for (const recordId of changedRecordIds) {
                         const record = this.parentRecordStore.getRecordByIdIfExists(recordId);
-                        if (!record) {
-                            throw spawnInvariantViolationError('record must exist');
+                        // Similar to above, we could be receiving the change notification
+                        // for a record color before receiving the new record itself.
+                        if (record) {
+                            record.__triggerOnChangeForRecordColorInViewId(this.viewId);
                         }
-                        record.__triggerOnChangeForRecordColorInViewId(this.viewId);
                     }
                 }
             }
