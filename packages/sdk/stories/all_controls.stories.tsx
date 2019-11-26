5e24d15f1b39a84f65a6702e21e2b3fef3d2c0f8
Stephen Suen <stephen.suen@airtable.com>
2019-11-26 14:10:30 -0800

    sdk: normalize control sizes
    Summary:
    Tweak control sizes so that they all have the same font size given the same `size` prop (except for `Button`, which is one step higher since the primary use case is for CTAs like "Done").
    
    This means that `SelectButtons`/`Switch` with `size="small"` will no longer reduce the font size.
    
    Before:
    
    {F146688}
    
    After:
    
    {F146687}
    
    Test Plan: Verify the "All controls" story in storybook.
    
    Reviewers: jay-ransijn, billylittlefield
    
    Reviewed By: billylittlefield
    
    Differential Revision: https://airtable.phacility.com/D13333

---
+++ packages/sdk/stories/all_controls.stories.tsx
@@ -0,0 +1,69 @@
+// @flow
+import React, {useState} from 'react';
+import {storiesOf} from '@storybook/react';
+import {values} from '../src/private_utils';
+import colors from '../src/colors';
+import {ControlSize} from '../src/ui/control_sizes';
+import Box from '../src/ui/box';
+import Button from '../src/ui/button';
+import Input from '../src/ui/input';
+import Select from '../src/ui/select';
+import SelectButtons from '../src/ui/select_buttons';
+import Switch from '../src/ui/switch';
+
+const stories = storiesOf('All controls', module);
+const sizes = [ControlSize.small, ControlSize.default, ControlSize.large];
+const options = ['Apple', 'Pear', 'Banana'].map(value => ({value, label: value}));
+
+function capitalize(str: string) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+stories.add('sizes', () => {
+    const [value, setValue] = useState('');
+    const [isChecked, setIsChecked] = useState(true);
+    const [selectValue, setSelectValue] = useState<string | null>(null);
+    return (
+        <>
+            {sizes.map(size => (
+                <Box display="flex" flexDirection="column" width="300px" marginBottom={5}>
+                    <Input
+                        marginBottom={2}
+                        size={size}
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        placeholder={`${capitalize(size)} input`}
+                    />
+                    <Select
+                        marginBottom={2}
+                        size={size}
+                        options={[{value: null, label: `${capitalize(size)} select`}, ...options]}
+                        value={selectValue}
+                        onChange={selectValue =>
+                            setSelectValue(selectValue ? (selectValue as string) : null)
+                        }
+                    />
+                    <SelectButtons
+                        marginBottom={2}
+                        size={size}
+                        options={options}
+                        value={selectValue}
+                        onChange={selectValue =>
+                            setSelectValue(selectValue ? (selectValue as string) : null)
+                        }
+                    />
+                    <Switch
+                        marginBottom={2}
+                        size={size}
+                        value={isChecked}
+                        onChange={setIsChecked}
+                        label={`${capitalize(size)} switch`}
+                    />
+                    <Button variant="primary" size={size}>
+                        {capitalize(size)} button
+                    </Button>
+                </Box>
+            ))}
+        </>
+    );
+});
