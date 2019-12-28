60b4b60f504584e6ab90441713e19da1ad033628
Ben Razon <ben.razon@airtable.com>
2019-12-27 17:20:31 -0800

    Add typescript hello world example block
    Summary: Fixes https://airtable.com/tblirJUiB3DryM26j/viweryBDQNKvrnKpA/recIbsvioaNxVl5DD?blocks=bipdCpWkCd7sxO47r
    
    Test Plan: Install as custom block and observe that it loads
    
    Reviewers: minicat, kasrak, somehats
    
    Reviewed By: kasrak
    
    Differential Revision: https://airtable.phacility.com/D13893

---
+++ examples/hello-world-typescript-block/frontend/index.tsx
@@ -0,0 +1,9 @@
+import {initializeBlock} from '@airtable/blocks/ui';
+import React from 'react';
+
+function HelloWorldTypescriptBlock() {
+    // YOUR CODE GOES HERE
+    return <div>Hello world 🚀</div>;
+}
+
+initializeBlock(() => <HelloWorldTypescriptBlock />);
