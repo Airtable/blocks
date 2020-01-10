f6d5baf8e8f665cf05fcaf8c723c22aefeb9fb36
Billy Littlefield <billy.littlefield@airtable.com>
2020-01-10 13:18:34 -0800

    sdk_ui: ViewPickerSynced live example
    Summary: {F153869}
    
    Test Plan: No QA needed
    
    Reviewers: jay-ransijn
    
    Reviewed By: jay-ransijn
    
    Differential Revision: https://airtable.phacility.com/D14236

--- packages/sdk/stories/model_pickers.stories.tsx
+++ packages/sdk/stories/model_pickers.stories.tsx
@@ -5,6 +5,7 @@ import useTheme from '../src/ui/theme/use_theme';
 import {keys} from '../src/private_utils';
 import Example from './helpers/example';
 import {createJsxPropsStringFromValuesMap, CONTROL_WIDTH} from './helpers/code_utils';
+import theme from '../src/ui/theme/default_theme';
 
 const stories = storiesOf('ModelPickers', module);
 
@@ -13,28 +14,30 @@ const viewOptions = ['All tasks', 'Grouped by status', 'Incomplete tasks'].map(v
     label: value,
 }));
 
+const sharedModelPickerOptions = {
+    size: {
+        type: 'selectButtons',
+        label: 'Size',
+        options: keys(theme.selectSizes),
+        defaultValue: 'default',
+    },
+    disabled: {
+        type: 'switch',
+        label: 'Disabled',
+        defaultValue: false,
+    },
+    shouldAllowPickingNone: {
+        type: 'switch',
+        label: 'Allow empty selection',
+        defaultValue: false,
+    },
+} as const;
+
 function ViewPickerExample() {
-    const {selectSizes} = useTheme();
     return (
         <Example
-            options={{
-                size: {
-                    type: 'selectButtons',
-                    label: 'Size',
-                    options: keys(selectSizes),
-                    defaultValue: 'default',
-                },
-                disabled: {
-                    type: 'switch',
-                    label: 'Disabled',
-                    defaultValue: false,
-                },
-                shouldAllowPickingNone: {
-                    type: 'switch',
-                    label: 'Allow empty selection',
-                    defaultValue: false,
-                },
-            }}
+            options={sharedModelPickerOptions}
+            styleProps={Object.keys(selectStylePropTypes)}
             renderCodeFn={values => {
                 const props = createJsxPropsStringFromValuesMap(values);
 
@@ -52,7 +55,6 @@ function ViewPickerExample() {
                     };
                 `;
             }}
-            styleProps={Object.keys(selectStylePropTypes)}
         >
             {values => {
                 const [value, setValue] = useState<null | string>(null);
@@ -79,4 +81,56 @@ function ViewPickerExample() {
     );
 }
 
-stories.add('viewpicker example', () => <ViewPickerExample />);
+stories.add('ViewPicker example', () => <ViewPickerExample />);
+
+function ViewPickerSyncedExample() {
+    return (
+        <Example
+            options={sharedModelPickerOptions}
+            styleProps={Object.keys(selectStylePropTypes)}
+            renderCodeFn={values => {
+                const props = createJsxPropsStringFromValuesMap(values);
+
+                return `
+                    import React from 'react';
+                    import {useBase, useGlobalConfig, ViewPickerSynced} from '@airtable/blocks/ui';
+
+                    const ViewPickerExample = () => {                    
+                        const globalConfig = useGlobalConfig();
+                        const base = useBase();
+                        const table = base.getTableByNameIfExists('Tasks');
+                        // If table is null or undefined, the ViewPickerSynced will not render.
+                        const viewId = globalConfig.get('viewId');
+                        const view = table.getViewByIdIfExists(viewId);
+
+                        return <ViewPickerSynced table={table} globalConfigKey='viewId' onChange={newView => setView(newView)} ${props} width="${CONTROL_WIDTH}"/>
+                    };
+                `;
+            }}
+        >
+            {values => {
+                const [value, setValue] = useState<null | string>(null);
+                const placeholder = values.shouldAllowPickingNone ? 'None' : 'Pick a view...';
+
+                return (
+                    <Select
+                        width={CONTROL_WIDTH}
+                        options={[
+                            {
+                                value: null,
+                                label: placeholder,
+                                disabled: !values.shouldAllowPickingNone,
+                            },
+                            ...viewOptions,
+                        ]}
+                        value={value}
+                        onChange={newValue => setValue(newValue as string)}
+                        {...values}
+                    />
+                );
+            }}
+        </Example>
+    );
+}
+
+stories.add('ViewPickerSynced example', () => <ViewPickerSyncedExample />);
