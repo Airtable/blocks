e5b7b69836867689051ca1f4ab0b0a853509277e
Richard Sinn <richard.sinn@airtable.com>
2020-02-23 22:10:58 -0800

    sdk: initial pipeline implementation to h.stats
    Summary:
    Depends on D15299 (in `hyperbase` repo)
    
    Why
    ===
    This is effectively a "forward-port" of D15299 to build out the stat pipeline
    for the new Blocks SDK.
    
    Docs:
    * **1st-Party Blocks Online Stat Tracking (Online Analytics)** - https://airtable.quip.com/TBS6AKDwiJy2
    * **[October 2019] Blocks Observability State of the Union** - https://airtable.quip.com/b4iBAeBtib10
    
    What
    ====
    Add a new private module called `block_stats`. The blocks code will be using this new module to call stat APIs.
    
    Example usage (in blocks code - not in this diff):
    ```
    import {blockStats} from '@airtable/blocks/unstable_private_utils';
    
    blockStats.increment('airtable.csv_import.new_blocks_sdk.w00tw00t');
    ```
    
    Tested manually (screenshots of datadog):
    Tested locally on hyperbasedev with local CSV Import block.
    * {F161780}
    * {F161781}
    
    Test Plan:
    No QA needed
    Tested manually
    
    Reviewers: somehats, minicat, billylittlefield
    
    Reviewed By: somehats
    
    Differential Revision: https://airtable.phacility.com/D15300

--- packages/sdk/unstable_private_utils.js
+++ packages/sdk/unstable_private_utils.js
@@ -3,4 +3,5 @@ module.exports = Object.assign(
     require('./dist/cjs/private_utils'),
     require('./dist/cjs/error_utils'),
     require('./dist/cjs/event_tracker'),
+    require('./dist/cjs/stats/block_stats'),
 );
