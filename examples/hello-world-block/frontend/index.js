e040d1633827bd139665dedeac3f7209edc3f79a
Billy Littlefield <billy.littlefield@airtable.com>
2020-02-27 18:16:07 -0800

    examples: add hello world
    Summary: Adds the hello world example block to the blocks-sdk/examples directory, and creates a base in the blocks construction workspace for it: https://airtable.com/tblJQKPGk1E3MPoih/viwvEvMPMfJ8p9TCz?blocks=bip1XqTDvuXCJB83o
    
    Test Plan: No qa needed
    
    Reviewers: minicat, s2tephen
    
    Reviewed By: s2tephen
    
    Subscribers: benrazon
    
    Differential Revision: https://airtable.phacility.com/D15454

---
+++ examples/hello-world-block/frontend/index.js
@@ -0,0 +1,9 @@
+import {initializeBlock} from '@airtable/blocks/ui';
+import React from 'react';
+
+function HelloWorldBlock() {
+    // YOUR CODE GOES HERE
+    return <div>Hello world 🚀</div>;
+}
+
+initializeBlock(() => <HelloWorldBlock />);
