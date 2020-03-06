Airtable/blocks

commit 940923b79a8b9798df5f78cf2c87acf3d9d032e5

Base Schema: Fix crashes when trying to set global config without permission
Summary: Disable dragging and be more careful on handling schema changes

Test Plan:
In advanced settings, switch to commenter or read-only
Try to drag tables around, also add and remove a table in another tab
to simulate another collaborator editing the schmema while viewing the block

Reviewers: jay-ransijn, minicat

Reviewed By: jay-ransijn

Subscribers: billylittlefield

Differential Revision: https://airtable.phacility.com/D15628

examples/base-schema-block/frontend/settings.js
@@ -55,6 +55,8 @@ export default function useSettings() {
     const [baseSchema, setBaseSchema] = useState(() => parseSchema(base));
     const {nodesById, linksById, tableConfigsByTableId, dependentLinksByNodeId} = baseSchema;
     const globalConfig = useGlobalConfig();
+    let tableCoordsByTableId = globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID);
+
     if (!globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID)) {
         // First time run, determine initial table coords
         globalConfig.setPathsAsync([
@@ -81,16 +83,15 @@ export default function useSettings() {
                 Object.keys(globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID)),
             ).length > 0
         ) {
-            globalConfig.setAsync(
-                ConfigKeys.TABLE_COORDS_BY_TABLE_ID,
-                getUpdatedTableCoords(
-                    tableConfigsByTableId,
-                    globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID),
-                ),
+            tableCoordsByTableId = getUpdatedTableCoords(
+                tableConfigsByTableId,
+                globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID),
             );
+            if (globalConfig.hasPermissionToSet()) {
+                globalConfig.setAsync(ConfigKeys.TABLE_COORDS_BY_TABLE_ID, tableCoordsByTableId);
+            }
         }
     }
-    const tableCoordsByTableId = globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID);
     const [linkPathsByLinkId, setLinkPathsByLinkId] = useState(() =>
         calculateLinkPaths(linksById, tableConfigsByTableId, tableCoordsByTableId),
     );
@@ -109,7 +110,9 @@ export default function useSettings() {
             newTableCoords,
         );
 
-        globalConfig.setAsync(ConfigKeys.TABLE_COORDS_BY_TABLE_ID, newTableCoords);
+        if (globalConfig.hasPermissionToSet()) {
+            globalConfig.setAsync(ConfigKeys.TABLE_COORDS_BY_TABLE_ID, newTableCoords);
+        }
         setBaseSchema(newSchema);
         setLinkPathsByLinkId(newLinkPaths);
     });

examples/base-schema-block/frontend/DragWrapper.js
@@ -42,6 +42,11 @@ export default function DragWrapper({
      */
     const handleTableDrag = useCallback(
         (event, tableId) => {
+            if (!globalConfig.hasPermissionToSet()) {
+                // Disable dragging for comment/read-only users
+                return;
+            }
+
             svgPanZoom.disablePan();
             const tableElement = event.currentTarget.parentElement;
             const activeItemContainerElement = document.getElementById('active-container');

examples/base-schema-block/frontend/SvgTable.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
+import {globalConfig} from '@airtable/blocks';
 import {colors, colorUtils} from '@airtable/blocks/ui';
 
 import {DragContext} from './DragWrapper';
@@ -42,6 +43,7 @@ export default function SvgTable({coords, tableConfig}) {
     const {tableId} = tableConfig.tableNode;
     const {x, y} = coords;
     const {handleTableDrag} = useContext(DragContext);
+    const canDrag = globalConfig.hasPermissionToSet();
 
     const tableHeight = ROW_HEIGHT * (tableConfig.fieldNodes.length + 1) + 2 * TABLE_BORDER_WIDTH;
     return (
@@ -58,6 +60,7 @@ export default function SvgTable({coords, tableConfig}) {
                 rowIndex={0}
                 node={tableConfig.tableNode}
                 onTableRowDrag={e => handleTableDrag(e, tableId)}
+                canDrag={canDrag}
             />
             {tableConfig.fieldNodes.map((fieldNode, index) => {
                 return (
@@ -67,6 +70,7 @@ export default function SvgTable({coords, tableConfig}) {
                         rowIndex={index + 1}
                         node={fieldNode}
                         onTableRowDrag={() => {}}
+                        canDrag={false}
                     />
                 );
             })}
@@ -111,8 +115,9 @@ function truncateTextForWidth(text, isHeader, width = ROW_WIDTH - 2 * TEXT_PADDI
  * @param {Object} node Node object containing name and relevant ids
  * @param {boolean} isHeader Whether this table row is a the table header
  * @param {onTableRowDrag} function mousedown event handler to control table dragging
+ * @param {boolean} canDrag should be true when onTableRowDrag is not a no-op
  */
-function TableRow({rowIndex, node, isHeader, onTableRowDrag}) {
+function TableRow({rowIndex, node, isHeader, onTableRowDrag, canDrag}) {
     const truncatedRowName = truncateTextForWidth(node.name, isHeader);
     // Give each table header a random, determinsitic color based off the tableId
     let headerColorString;
@@ -124,6 +129,7 @@ function TableRow({rowIndex, node, isHeader, onTableRowDrag}) {
         <svg
             className={classnames('TableRow', {
                 TableHeader: isHeader,
+                draggable: canDrag,
             })}
             id={node.id}
             x={TABLE_BORDER_WIDTH} // give room for filter box-shadow

examples/base-schema-block/frontend/loadCss.js
@@ -38,13 +38,16 @@ const css = `
     }
 
     .TableRow.TableHeader {
-        cursor: grab;
         stroke-width: 0;
         font-weight: 600;
     }
 
     .TableRow.TableHeader text {
         fill: #ffffff;
+    }
+    
+    .TableRow.draggable {
+        cursor: grab;
     }
 
     .Link {

commit 61a0c377bbf058914595a81fa0c61f3ebedea2bc

base schema: fix crash on install
Summary:
https://airtable.com/tblJvlqwVOgRLmgdT/viwmUCiEmmQdVfxND/rec9K5QJynQwufx0Q?blocks=hide
I broke installing the block in D15628 since on first run tableCoordsByTableId wouldn't
get set.

Test Plan: Install the release version of the block. It should no longer crash

Reviewers: minicat, jay-ransijn

Reviewed By: jay-ransijn

Differential Revision: https://airtable.phacility.com/D15750

examples/base-schema-block/frontend/settings.js
@@ -55,14 +55,15 @@ export default function useSettings() {
     const [baseSchema, setBaseSchema] = useState(() => parseSchema(base));
     const {nodesById, linksById, tableConfigsByTableId, dependentLinksByNodeId} = baseSchema;
     const globalConfig = useGlobalConfig();
-    let tableCoordsByTableId = globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID);
+    let tableCoordsByTableId;
 
     if (!globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID)) {
         // First time run, determine initial table coords
+        tableCoordsByTableId = getInitialTableCoords(tableConfigsByTableId);
         globalConfig.setPathsAsync([
             {
                 path: [ConfigKeys.TABLE_COORDS_BY_TABLE_ID],
-                value: getInitialTableCoords(tableConfigsByTableId),
+                value: tableCoordsByTableId,
             },
             {
                 path: [ConfigKeys.ENABLED_LINKS_BY_TYPE],
@@ -77,15 +78,14 @@ export default function useSettings() {
         ]);
     } else {
         // Non-first time run, check for any new tables missing from the old saved coords
+        tableCoordsByTableId = globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID);
         if (
-            _.difference(
-                Object.keys(tableConfigsByTableId),
-                Object.keys(globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID)),
-            ).length > 0
+            _.difference(Object.keys(tableConfigsByTableId), Object.keys(tableCoordsByTableId))
+                .length > 0
         ) {
             tableCoordsByTableId = getUpdatedTableCoords(
                 tableConfigsByTableId,
-                globalConfig.get(ConfigKeys.TABLE_COORDS_BY_TABLE_ID),
+                tableCoordsByTableId,
             );
             if (globalConfig.hasPermissionToSet()) {
                 globalConfig.setAsync(ConfigKeys.TABLE_COORDS_BY_TABLE_ID, tableCoordsByTableId);