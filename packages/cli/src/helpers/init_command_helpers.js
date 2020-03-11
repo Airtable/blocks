f2531c11b18d85b03702443cc5f1844c186dacfb
Ben Razon <ben.razon@airtable.com>
2020-03-11 16:58:39 -0700

    Cli: Don't create a git repo when initializing a block
    Summary:
    https://airtable.com/tblirJUiB3DryM26j/viweryBDQNKvrnKpA/rec2H9mOlzeJLQJwH?blocks=bipdCpWkCd7sxO47r
    Users might find the git history confusing when starting a new block especially since we make some
    uncommitted changes and the git remote isn't one that they can push to.
    This just deletes the .git directory that git clone creates.
    I also removed some logging
    
    Test Plan: Run the tests. Also init a block and check that it is no longer a git repo.
    
    Reviewers: richsinn, minicat
    
    Reviewed By: minicat
    
    Differential Revision: https://airtable.phacility.com/D15829

--- packages/cli/src/helpers/init_command_helpers.js
+++ packages/cli/src/helpers/init_command_helpers.js
@@ -6,7 +6,10 @@ const {npmAsync, gitAsync} = require('./node_modules_command_helpers');
 const TMP_DIRECTORY_NAME = 'tmp';
 
 async function downloadTemplateAsync(blockDirPath: string, template: string): Promise<string> {
-    await gitAsync(blockDirPath, ['clone', template, TMP_DIRECTORY_NAME]);
+    await gitAsync(blockDirPath, ['clone', '--quiet', template, TMP_DIRECTORY_NAME]);
+    // We remove the .git directory since users might find the git history confusing especially
+    // since we make some uncommitted changes and the git remote isn't one they can push to.
+    await fsUtils.removeAsync(path.join(blockDirPath, TMP_DIRECTORY_NAME, '.git'));
     // Maybe add a flag to handle blocks that aren't at the top level of the repo?
     return path.join(blockDirPath, TMP_DIRECTORY_NAME);
 }
