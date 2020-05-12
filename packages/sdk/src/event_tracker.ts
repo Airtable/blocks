66600f12330ece38eb1bd43e6202f29415227284
Emma Yeap <emma@airtable.com>
2020-05-12 17:33:10 +0800

    sdk: add RecordActionStatus to unstable_private_utils
    Summary:
    Mirrors enum from D18018
    
    Adding to unstable_private_utils so our blocks can use the enum instead of hard-coding the various values into each block.
    
    We can land this ahead of the 5/20 hyperbase deploy, but the logging wont work until then.
    
    Test Plan:
    I tested that, after installing the local version of the SDK, you can import it from `unstable_private_utils`:
    
    {F183689}
    
    Reviewers: richsinn
    
    Reviewed By: richsinn
    
    Subscribers: jay-ransijn
    
    Differential Revision: https://airtable.phacility.com/D18122

--- packages/sdk/src/event_tracker.ts
+++ packages/sdk/src/event_tracker.ts
@@ -3,3 +3,25 @@ import getSdk from './get_sdk';
 export function trackEvent(eventSchemaName: string, eventData: {[key: string]: unknown} = {}) {
     getSdk().__airtableInterface.trackEvent(eventSchemaName, eventData);
 }
+
+/**
+ * Used for logging blockInstallation.performRecordAction event
+ * Mirrored from hyperbase block_installation_record_action_status.tsx
+ *
+ * @hidden
+ * */
+export enum RecordActionStatus {
+    // The record was successfully opened.
+    SUCCESS = 'SUCCESS',
+    // The request was ignored, due to the block being in a state where it isn't ready to handle events
+    // e.g. setup, settings
+    // Note: in the future, we may handle events from some of these states or show the user an error
+    // message.
+    IGNORED = 'IGNORED',
+    // Due to invalid settings, it wasn't possible to handle the event.
+    // Unlike IGNORED, we show the user an error message.
+    SETTINGS_INVALID = 'SETTINGS_INVALID',
+    // The block was opened from a table different to the one the block is configured to use.
+    // Unlike IGNORED, we show the user an error message.
+    TABLE_MISMATCH = 'TABLE_MISMATCH',
+}
