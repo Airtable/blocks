d810a0338dcf988bf0ddfeec1660b665feaf2bff
Rebecca Meritz <rebecca@meritz.com>
2020-07-16 12:48:23 -0700

    Remove git as a dependency
    Summary:
    Replace with github API to download repo.
    https://developer.github.com/v3/repos/contents/#download-a-repository-archive
    https://octokit.github.io/rest.js/v18#repos-download-archive
    
    And tar to untar repo.
    https://www.npmjs.com/package/tar
    
    Test Plan: Run block init with different template
    
    Reviewers: richsinn, minicat
    
    Reviewed By: richsinn
    
    Differential Revision: https://airtable.phacility.com/D20232

--- packages/cli/src/helpers/init_command_helpers.js
+++ packages/cli/src/helpers/init_command_helpers.js
@@ -1,17 +1,48 @@
 // @flow
 const path = require('path');
 const fsUtils = require('./fs_utils');
-const {npmAsync, gitAsync} = require('./node_modules_command_helpers');
+const {createWriteStream} = require('fs');
+const {npmAsync} = require('./node_modules_command_helpers');
+const {Octokit} = require('@octokit/rest');
+const request = require('postman-request');
+const tar = require('tar');
 
 const TMP_DIRECTORY_NAME = 'tmp';
+const TARBALL_FILE_NAME = 'template.tar.gz';
 
 async function downloadTemplateAsync(blockDirPath: string, template: string): Promise<string> {
-    await gitAsync(blockDirPath, ['clone', '--quiet', template, TMP_DIRECTORY_NAME]);
-    // We remove the .git directory since users might find the git history confusing especially
-    // since we make some uncommitted changes and the git remote isn't one they can push to.
-    await fsUtils.removeAsync(path.join(blockDirPath, TMP_DIRECTORY_NAME, '.git'));
-    // Maybe add a flag to handle blocks that aren't at the top level of the repo?
-    return path.join(blockDirPath, TMP_DIRECTORY_NAME);
+    const tmpDirPath = path.join(blockDirPath, TMP_DIRECTORY_NAME);
+    const tarballPath = path.join(blockDirPath, TARBALL_FILE_NAME);
+    const writeStream = createWriteStream(tarballPath, {encoding: null});
+    await fsUtils.mkdirAsync(tmpDirPath);
+
+    let downloadUrl;
+    const octokit = new Octokit({});
+    const pathComponents = new URL(template).pathname.split('/');
+    const owner = pathComponents[1];
+    const repo = pathComponents[2];
+    try {
+        const {url} = await octokit.repos.downloadArchive({owner, repo, archive_format: 'tarball'});
+        downloadUrl = url;
+    } catch (e) {
+        if (e.status === 404) {
+            e.message = `Template ${template} is not a public repo that could be found on Github`;
+        }
+        throw e;
+    }
+
+    await new Promise((resolve, reject) => {
+        request(downloadUrl)
+            .pipe(writeStream)
+            .on('finish', resolve)
+            .on('error', reject);
+    });
+
+    await tar.extract({file: tarballPath, gzip: true, cwd: tmpDirPath});
+    await fsUtils.unlinkAsync(tarballPath);
+    const files = await fsUtils.readDirAsync(tmpDirPath, {withFileTypes: true, encoding: 'utf8'});
+    const templateDir = files.find(dirent => dirent.isDirectory());
+    return path.join(tmpDirPath, templateDir.name);
 }
 
 async function cleanUpDownloadedTemplateAsync(blockDirPath: string): Promise<void> {
