9a1f1fa69afb60c465389d00df83f546d51d34b5
Mike Pennisi <mike@mikepennisi.com>
2020-10-15 14:13:48 -0400

    sdk: Add further tests for hooks
    Test Plan: n/a (this changeset concerns the tests themselves)
    
    Reviewers: richsinn, somehats
    
    Reviewed By: somehats
    
    Differential Revision: https://airtable.phacility.com/D24065

--- packages/sdk/test/ui/use_records.test.tsx
+++ packages/sdk/test/ui/use_records.test.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import {act} from 'react-dom/test-utils';
 import {useRecords} from '../../src/ui/use_records';
 
+import getSdk from '../../src/get_sdk';
 import MockAirtableInterface from '../airtable_interface_mocks/mock_airtable_interface';
 import Base from '../../src/models/base';
 import Record from '../../src/models/record';
 import Table from '../../src/models/table';
+import {TableId} from '../../src/types/table';
 
-const mockAirtableInterface = MockAirtableInterface.projectTrackerExample();
+const mockAirtableInterface = MockAirtableInterface.linkedRecordsExample();
 jest.mock('../../src/injected/airtable_interface', () => ({
     __esModule: true,
     default: () => mockAirtableInterface,
@@ -16,24 +18,26 @@ jest.mock('../../src/injected/airtable_interface', () => ({
 
 describe('useRecords', () => {
     let table: Table;
+    const createdTime = new Date().toString();
+    const recordsById = {
+        recA: {id: 'recA', cellValuesByFieldId: {}, commentCount: 0, createdTime},
+        recB: {id: 'recB', cellValuesByFieldId: {}, commentCount: 0, createdTime},
+        recC: {id: 'recC', cellValuesByFieldId: {}, commentCount: 0, createdTime},
+    };
+    const Component = ({target, resolve}: {target: any; resolve: Function}) => {
+        const records = useRecords(target);
+        resolve(records);
+        return <div></div>;
+    };
+    const visibleRecordIds = ['recA', 'recC'];
+
     beforeEach(() => {
         mockAirtableInterface.reset();
         let base = new Base(mockAirtableInterface.sdkInitData.baseData, mockAirtableInterface);
-        table = base.getTableByName('Design projects');
+        table = base.getTableByName('First Table');
     });
 
-    it('eventually returns all records', async () => {
-        const Component = ({resolve}: {resolve: Function}) => {
-            const records = useRecords(table);
-            resolve(records);
-            return <div></div>;
-        };
-        const createdTime = new Date().toString();
-        const recordsById = {
-            recA: {id: 'recA', cellValuesByFieldId: {}, commentCount: 0, createdTime},
-            recB: {id: 'recB', cellValuesByFieldId: {}, commentCount: 0, createdTime},
-            recC: {id: 'recC', cellValuesByFieldId: {}, commentCount: 0, createdTime},
-        };
+    it('eventually returns all records from a table', async () => {
         mockAirtableInterface.fetchAndSubscribeToTableDataAsync.mockResolvedValue({recordsById});
         mockAirtableInterface.fetchAndSubscribeToCellValuesInFieldsAsync.mockResolvedValue({
             recordsById,
@@ -44,7 +48,7 @@ describe('useRecords', () => {
             act(() => {
                 ReactDOM.render(
                     <Suspense fallback="">
-                        <Component resolve={resolve} />
+                        <Component target={table} resolve={resolve} />
                     </Suspense>,
                     document.createElement('div'),
                 );
@@ -53,5 +57,81 @@ describe('useRecords', () => {
 
         const ids = records.map(({id}) => id).sort();
         expect(ids).toStrictEqual(['recA', 'recB', 'recC']);
+    });
+
+    it('eventually returns visible records from a view', async () => {
+        mockAirtableInterface.fetchAndSubscribeToTableDataAsync.mockResolvedValue({recordsById});
+        mockAirtableInterface.fetchAndSubscribeToViewDataAsync.mockResolvedValue({
+            visibleRecordIds,
+            fieldOrder: {
+                fieldIds: [],
+                visibleFieldCount: 0,
+            },
+            colorsByRecordId: {},
+        });
+        mockAirtableInterface.fetchAndSubscribeToCellValuesInFieldsAsync.mockResolvedValue({
+            recordsById,
+        });
+
+        // Enzyme doesn't support <Suspense /> yet, so use ReactDOM manually.
+        const records: Record[] = await new Promise(resolve => {
+            act(() => {
+                ReactDOM.render(
+                    <Suspense fallback="">
+                        <Component target={table.views[0]} resolve={resolve} />
+                    </Suspense>,
+                    document.createElement('div'),
+                );
+            });
+        });
+
+        const ids = records.map(({id}) => id).sort();
+        expect(ids).toStrictEqual(['recA', 'recC']);
+    });
+
+    it('eventually returns records from a LinkedRecordsQueryResult', async () => {
+        const impl = (tableId: TableId) => {
+            const first = tableId === 'tblFirst';
+            const recId = first ? 'recA' : 'recD';
+            const fieldId = first ? 'fldLinked1' : 'fldLinked2';
+            const cellValues = [{id: first ? 'recD' : 'recA'}];
+            return Promise.resolve({
+                recordsById: {
+                    [recId]: {
+                        id: recId,
+                        cellValuesByFieldId: {
+                            fldPrimary: 'primary',
+                            [fieldId]: cellValues,
+                        },
+                        commentCount: 0,
+                        createdTime,
+                    },
+                },
+            });
+        };
+        mockAirtableInterface.fetchAndSubscribeToTableDataAsync.mockImplementation(impl);
+        mockAirtableInterface.fetchAndSubscribeToCellValuesInFieldsAsync.mockImplementation(impl);
+
+        const sdk = getSdk();
+        const query = await sdk.base.tables[0].selectRecordsAsync();
+        const record = query.getRecordById('recA');
+
+        // Enzyme doesn't support <Suspense /> yet, so use ReactDOM manually.
+        const records: Record[] = await new Promise(resolve => {
+            act(() => {
+                ReactDOM.render(
+                    <Suspense fallback="">
+                        <Component
+                            target={record.selectLinkedRecordsFromCell('fldLinked1')}
+                            resolve={resolve}
+                        />
+                    </Suspense>,
+                    document.createElement('div'),
+                );
+            });
+        });
+
+        const ids = records.map(({id}) => id).sort();
+        expect(ids).toStrictEqual(['recD']);
     });
 });
