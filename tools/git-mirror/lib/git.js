3796bd57d0999a641bd725a3646746b436668186
Douglas Forster <doug+ops@airtable.com>
2021-01-22 00:57:28 +0000

    update comment
    Summary:
    update comment
    
    the purpose is to test whether Phabricator automatically closes the diff.
    
    Test Plan: No QA needed
    
    Reviewers: benrazon
    
    Reviewed By: benrazon
    
    Differential Revision: https://airtable.phacility.com/D27202

--- tools/git-mirror/lib/git.js
+++ tools/git-mirror/lib/git.js
@@ -71,7 +71,8 @@ module.exports = {
     async pushAsync(cwd) {
         // This pushes to the the public mirror at https://github.com/airtable/blocks,
         // FIXME_MAIN update the default branch of that public-facing repository; it'll
-        // be tricky. Details/discussion are in this doc: https://docs.google.com/document/d/10nzMV73L5pl2cXRu1GKTepSO4gMkWss4Hw3YWiDkBbg/edit#heading=h.urgi97elgt9t
+        // be tricky. Details/discussion are in this doc:
+        // https://docs.google.com/document/d/10nzMV73L5pl2cXRu1GKTepSO4gMkWss4Hw3YWiDkBbg/edit#heading=h.urgi97elgt9t
         await runGitAsync(cwd, 'push', '--tags', 'origin', 'master');
     },
 };
