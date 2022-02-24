7456508e799812daf3d82817074b75bdbd3a2fcc
Kevin Wilde <kevin.wilde@airtable.com>
2022-02-24 12:02:30 -0800

    eslint-plugin-blocks: Copy over implementation getStaticPropertyName instead of using internal eslint file (#139)
    # Summary
    Slack thread: https://h.slack.com/archives/C018YBVKELD/p1644598968151529
    `@airtable/eslint-plugin-blocks` does not play nice with the latest version of eslint because we were relying on an internal file in the package. This copies over the implementation of the function we needed rather than importing it.
    
    The plan is to publish a new version of `@airtable/eslint-plugin-blocks` and then update usages in hyperbase to unblock the hyperbase eslint upgrade.
    
    ## Tasks
    - Add to the changelog if needed? I.e. [the SDK changelog](packages/sdk/CHANGELOG.md)
      - [x] Not needed
      - [ ] Added in this PR
    
    
    
    # Test Plan
    Automated tests
    
    
    Reviewed By: JamesMoody-at
    GitHub Pull Request: https://github.com/Hyperbase/blocks-sdk/pull/139

--- packages/eslint-plugin-blocks/lib/rules/no-error-interpolation.js
+++ packages/eslint-plugin-blocks/lib/rules/no-error-interpolation.js
@@ -1,10 +1,128 @@
-const getPropertyName = require('eslint/lib/rules/utils/ast-utils').getStaticPropertyName;
+// isNullLiteral, getStaticStringValue and getStaticPropertyName are taken from
+// https://github.com/eslint/eslint/blob/fd3683f/lib/rules/utils/ast-utils.js#L15ß0-L290
+
+/**
+ * Determines whether the given node is a `null` literal.
+ * @param {ASTNode} node The node to check
+ * @returns {boolean} `true` if the node is a `null` literal
+ */
+function isNullLiteral(node) {
+    /*
+     * Checking `node.value === null` does not guarantee that a literal is a null literal.
+     * When parsing values that cannot be represented in the current environment (e.g. unicode
+     * regexes in Node 4), `node.value` is set to `null` because it wouldn't be possible to
+     * set `node.value` to a unicode regex. To make sure a literal is actually `null`, check
+     * `node.regex` instead. Also see: https://github.com/eslint/eslint/issues/8020
+     */
+    return node.type === 'Literal' && node.value === null && !node.regex && !node.bigint;
+}
+
+/**
+ * Returns the result of the string conversion applied to the evaluated value of the given expression node,
+ * if it can be determined statically.
+ *
+ * This function returns a `string` value for all `Literal` nodes and simple `TemplateLiteral` nodes only.
+ * In all other cases, this function returns `null`.
+ * @param {ASTNode} node Expression node.
+ * @returns {string|null} String value if it can be determined. Otherwise, `null`.
+ */
+function getStaticStringValue(node) {
+    switch (node.type) {
+        case 'Literal':
+            if (node.value === null) {
+                if (isNullLiteral(node)) {
+                    return String(node.value); // "null"
+                }
+                if (node.regex) {
+                    return `/${node.regex.pattern}/${node.regex.flags}`;
+                }
+                if (node.bigint) {
+                    return node.bigint;
+                }
+
+                // Otherwise, this is an unknown literal. The function will return null.
+            } else {
+                return String(node.value);
+            }
+            break;
+        case 'TemplateLiteral':
+            if (node.expressions.length === 0 && node.quasis.length === 1) {
+                return node.quasis[0].value.cooked;
+            }
+            break;
+
+        // no default
+    }
+
+    return null;
+}
+
+/**
+ * Gets the property name of a given node.
+ * The node can be a MemberExpression, a Property, or a MethodDefinition.
+ *
+ * If the name is dynamic, this returns `null`.
+ *
+ * For examples:
+ *
+ *     a.b           // => "b"
+ *     a["b"]        // => "b"
+ *     a['b']        // => "b"
+ *     a[`b`]        // => "b"
+ *     a[100]        // => "100"
+ *     a[b]          // => null
+ *     a["a" + "b"]  // => null
+ *     a[tag`b`]     // => null
+ *     a[`${b}`]     // => null
+ *
+ *     let a = {b: 1}            // => "b"
+ *     let a = {["b"]: 1}        // => "b"
+ *     let a = {['b']: 1}        // => "b"
+ *     let a = {[`b`]: 1}        // => "b"
+ *     let a = {[100]: 1}        // => "100"
+ *     let a = {[b]: 1}          // => null
+ *     let a = {["a" + "b"]: 1}  // => null
+ *     let a = {[tag`b`]: 1}     // => null
+ *     let a = {[`${b}`]: 1}     // => null
+ * @param {ASTNode} node The node to get.
+ * @returns {string|null} The property name if static. Otherwise, null.
+ */
+function getStaticPropertyName(node) {
+    let prop;
+
+    switch (node && node.type) {
+        case 'ChainExpression':
+            return getStaticPropertyName(node.expression);
+
+        case 'Property':
+        case 'PropertyDefinition':
+        case 'MethodDefinition':
+            prop = node.key;
+            break;
+
+        case 'MemberExpression':
+            prop = node.property;
+            break;
+
+        // no default
+    }
+
+    if (prop) {
+        if (prop.type === 'Identifier' && !node.computed) {
+            return prop.name;
+        }
+
+        return getStaticStringValue(prop);
+    }
+
+    return null;
+}
 
 function getNodeName(node) {
     if (node.type === 'Identifier') {
         return node.name;
     }
-    return getPropertyName(node);
+    return getStaticPropertyName(node);
 }
 
 module.exports = {
