bae408a216dc61c8bbcf41734de52cba55467277
richsinn <richsinn@users.noreply.github.com>
2023-06-23 18:55:19 -0700

    name_quiz: final commit for sunsetting block before code deletion (#225)
    # Summary
    
    Related PRs: 
    * https://github.com/Hyperbase/blocks-sdk/pull/215
    * https://github.com/Hyperbase/blocks-sdk/pull/213
    
    We are removing five low-used, low-impact 1P extensions to help with maintenance and security. See details here: https://docs.google.com/document/d/1Qus8DQ1RTsEGGneTUscckZ5C2skNOFMxQ51cHs-Z1_M/edit#
    
    NOTE: this is also an open source extension (https://github.com/Airtable/apps-name-quiz). We will still keep around the open source repo, but will delete this code from the `blocks-sdk` repo in a subsequent PR. 
    
    In a prior PR #213, we added the initial deprecation banner notifying users of pending removal. 
    
    In this PR, we are removing the functionality of this block, and replacing it with a final message notifying users that this block has been permanently removed.  We will. 
    1. Release and publish these changes to remove functionality for all existing users
    2. De-list the extension from the marketplace https://airtable.com/marketplace/blk7qrXbx1ivWn9Mr/name-quiz
    3. Remove the support article https://support.airtable.com/docs/en/name-quiz-extension
    
    In a subsequent PR, after we publish these changes, we will delete this directory and code from the hyperbase repo. 
    
    Screenshot of message:
    <img width="540" alt="image" src="https://github.com/Hyperbase/blocks-sdk/assets/726490/ad19de32-a073-41b8-a889-4c9bb7f88454">
    
    
    
    # Test Plan
    * Navigate to an existing Name quiz block installation
    * Notice the message showing removal of the block. The block should no longer be functional. 
    
    
    # QA Test Plan
    Manual QA Required ❌
    
    
    Reviewed By: NabeelFarooqui-at, HongdaJiang-at
    GitHub Pull Request: https://github.com/Hyperbase/blocks-sdk/pull/225

--- examples/name-quiz-block/frontend/index.js
+++ examples/name-quiz-block/frontend/index.js
@@ -1,58 +1,9 @@
-import React, {useState} from 'react';
-import {viewport, runInfo} from '@airtable/blocks';
-import {initializeBlock, Icon, colorUtils, colors} from '@airtable/blocks/ui';
-
-import SettingsForm from './SettingsForm';
-import HomeScreen from './HomeScreen';
-import GameCompletedScreen from './GameCompletedScreen';
-import Game from './Game';
-import loadCSS from './loadCSS';
-import FullScreenBox from './FullScreenBox';
+import React from 'react';
+import {initializeBlock} from '@airtable/blocks/ui';
 
 // Add the baymax class to the root to use the global baymax styles.
 // This was previously the default, but is now opt-in.
 document.body.classList.add('baymax');
-
-// Load all the CSS used in our extension.
-loadCSS();
-
-// Determines the minimum size the extension needs before it asks the user to enlarge the extension.
-viewport.addMinSize({
-    height: 520,
-    width: 600,
-});
-
-// Determines the maximum size of the extension in fullscreen mode.
-viewport.addMaxFullscreenSize({
-    height: 740,
-    width: 800,
-});
-
-/**
- * The game state determines what screen to render.
- */
-const GameStates = Object.freeze({
-    /**
-     * The player is configuring settings to be able to play the game.
-     * If the game is configured, other users will see the home screen instead.
-     */
-    CONFIGURING_SETTINGS: 'configuringSettings',
-    /**
-     * The player is viewing the home screen and is able to start the game,
-     * or when settings are invalid update the settings.
-     */
-    HOME_SCREEN: 'homeScreen',
-    /**
-     * The player is playing the game, any update in settings or any update to settings,
-     * the records or the base, will not interrupt the game.
-     */
-    PLAYING: 'playing',
-    /**
-     * The player completed the game, they either picked all correctly and won the game, or failed.
-     * They can go again or update the settings.
-     */
-    GAME_COMPLETED: 'gameCompleted',
-});
 
 /**
  * The name trainer extension is a game that shows a single name for multiple pictures.
@@ -61,105 +12,11 @@ const GameStates = Object.freeze({
  * This component handles all of the game lifecycle and renders a component based on the state of the game.
  */
 function NameQuizExtension() {
-    const [gameData, setGameData] = useState({
-        // On first run of the extension show the settings screen.
-        gameState: runInfo.isFirstRun ? GameStates.CONFIGURING_SETTINGS : GameStates.HOME_SCREEN,
-        // The game report will be populated when a game ends. It is used by the `GameCompletedScreen`.
-        gameReport: null,
-        // The list of names with pictures the game will use.
-        listOfNamesWithPictures: null,
-    });
-
-    const {gameState, gameReport, listOfNamesWithPictures} = gameData;
-
-    /**
-     * Start the game with a list of names with pictures.
-     *
-     * @param {Array<{recordId: string, name: string, largePictureUrl: string, smallPictureUrl: string}>} listOfNamesWithPictures
-     */
-    function startGame(listOfNamesWithPictures) {
-        // Enter the extension in fullscreen to have more real estate to play the game.
-        viewport.enterFullscreenIfPossible();
-        setGameData({
-            gameState: GameStates.PLAYING,
-            gameReport: null,
-            listOfNamesWithPictures,
-        });
-    }
-
-    function gameCompleted(newGameReport) {
-        setGameData({
-            gameState: GameStates.GAME_COMPLETED,
-            gameReport: newGameReport,
-            listOfNamesWithPictures: null,
-        });
-    }
-
-    function showSettings() {
-        setGameData({
-            gameState: GameStates.CONFIGURING_SETTINGS,
-            gameReport: null,
-            listOfNamesWithPictures: null,
-        });
-    }
-
-    function showHomeScreen() {
-        setGameData({
-            gameState: GameStates.HOME_SCREEN,
-            gameReport: null,
-            listOfNamesWithPictures: null,
-        });
-    }
-
-    function  _renderDeprecationBanner() {
-        return (
-            <div className="flex p1 red-light2 pb1 pt1">
-                <div className="ml1 flex flex-column justify-start">
-                    <Icon name="warning" fillColor={colorUtils.getHexForColor(colors.RED_BRIGHT)} />
-                </div>
-                <div className="ml1">
-                    <p className="strong mb1">
-                        We have removed support for this extension, but it is still usable through
-                        June 24, 2023. After June 24, 2023, we will permanently remove this extension.
-                    </p>
-                </div>
-            </div>
-        );
-    }
-
-    function _gameMode() {
-        switch (gameState) {
-            case GameStates.CONFIGURING_SETTINGS:
-                return <SettingsForm onDone={showHomeScreen} />;
-            case GameStates.PLAYING:
-                return (
-                    <Game
-                        listOfNamesWithPictures={listOfNamesWithPictures}
-                        onComplete={gameCompleted}
-                    />
-                );
-            case GameStates.HOME_SCREEN:
-                return <HomeScreen onStartGame={startGame} onShowSettings={showSettings} />;
-            case GameStates.GAME_COMPLETED:
-                return (
-                    <GameCompletedScreen
-                        gameReport={gameReport}
-                        onStartGame={startGame}
-                        onShowSettings={showSettings}
-                    />
-                );
-            default:
-                throw new Error('Unexpected game state: ', gameState);
-        }
-    }
-
     return (
-        <FullScreenBox display="flex" flexDirection="column">
-            {_renderDeprecationBanner()}
-            {_gameMode()}
-        </FullScreenBox>
-    )
-   
+        <div className="absolute all-0 flex flex-column items-center justify-center center darken1 p2">
+            Beginning June 24, 2023, the Name quiz extension is no longer available or supported.
+        </div>
+    );
 }
 
 initializeBlock(() => <NameQuizExtension />);
