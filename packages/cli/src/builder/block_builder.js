4288fa2e11070ef96218712637190119cdf56e8d
NabeelFarooqui-at <nabeel.f@airtable.com>
2023-06-30 19:02:38 -0400

    Add babel plugins to legacy cli (#227)
    # Summary
    Releasing blocks with a locally built cli results in frontend code
    transpilation errors -- `Missing class properties transform`, `Support
    for the experimental syntax 'optionalChaining' isn't currently enabled`,
    and `Support for the experimental syntax 'nullishCoalescingOperator'
    isn't currently enabled` -- this PR adds those plugins explicitly so
    that they run before the presets.
    
    Others have also faced this issue:
    [slack](https://h.slack.com/archives/C011FR9PVFT/p1676326187262689)
    
    ## Tasks
    
    - Add to the changelog if needed? I.e. [the SDK
    changelog](packages/sdk/CHANGELOG.md) or
        [the CLI changelog](packages/cli-next/CHANGELOG.md)
        -   [x] Not needed
        -   [ ] Added in this PR
    
    # Test Plan
    Successful local and staging releases of multiple blocks (gantt, org
    chart, search, map, world clock)

--- packages/cli/src/builder/block_builder.js
+++ packages/cli/src/builder/block_builder.js
@@ -505,7 +505,12 @@ class BlockBuilder {
             '@babel/preset-react',
             '@babel/preset-typescript',
         ];
-        const plugins = ['@babel/plugin-transform-flow-strip-types'];
+        const plugins = [
+            '@babel/plugin-transform-flow-strip-types',
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-nullish-coalescing-operator',
+            '@babel/plugin-proposal-optional-chaining',
+        ];
 
         // Use the blocks-cli dir as the cwd so babel can properly find presets/plugins.
         return await babel.transformFileAsync(filePath, {
