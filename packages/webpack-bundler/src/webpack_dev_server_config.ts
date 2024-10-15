599b83a0f932208c6afa8df3477ef47ae733e65e
richsinn <richsinn@users.noreply.github.com>
2024-10-14 18:33:57 -0700

    handle some more security vuln, esp webpack-bundler vuln (#248)
    # Summary
    Handle some more dependency vulnerabilities by upgrading them. For this
    PR, the biggest update was for `packages/webpack-bundler` where we
    upgraded `webpack-dev-server` from v3 to v4.
    
    We had to follow the following migraiton guide:
    https://github.com/webpack/webpack-dev-server/blob/629ecb20f6e1a080fc2f741857c7395e443bc1f9/migration-v4.md
    * `sockWrite` is removed in favor of `sendMessage`
    * `listen` is removed in favor of async `start`
    * Changes to `Configuration` object
    * etc
    
    
    # Test Plan
    
    Just relying on the existing automated tests. Haven't really tested it
    out.

--- packages/webpack-bundler/src/webpack_dev_server_config.ts
+++ packages/webpack-bundler/src/webpack_dev_server_config.ts
@@ -6,20 +6,12 @@ export interface WebpackDevServerOptions {
 }
 
 export function createWebpackDevServerConfig({port}: WebpackDevServerOptions): Configuration {
-    const unspecifiedConfiguration = {
-        // The `quiet` option causes the initial status to still be printed ...
-        // `logLevel` is more reliable. But its not specified in the
-        // configuration type.
-        logLevel: 'silent',
-    };
     return {
         port,
         // The client will be injected manually in the webpack configuration.
-        injectClient: false,
+        client: false,
         // Disable hot module replacement, we will just use live reload for the moment.
         hot: false,
-        disableHostCheck: true,
-        clientLogLevel: 'silent',
-        ...unspecifiedConfiguration,
+        allowedHosts: 'all',
     };
 }
